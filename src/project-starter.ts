921118bb7f6139e8bc9c123277816a7ddb9993c0
Burak Unver <burakhanunver@gmail.com>
2025-07-12 18:59:49 +0300

    format code

--- src/project-starter.ts
+++ src/project-starter.ts
@@ -1,28 +1,27 @@
-import { createReactProject } from "./helpers/create-react"
+import { createReactProject } from "./starters/create-react"
 
-export const projects = ["react-ts-tw","next-ts","wxt-react","wxt-svelte"]
-export const packageManagers = ["pnpm","npm","bun"]
-
+export const projects = ["react-ts-tw", "next-ts", "wxt-react", "wxt-svelte"]
+export const packageManagers = ["pnpm", "npm", "bun"]
 
 type options = {
-    projectType: typeof projects[number],
-    packageManager: typeof packageManagers[number]
-    name: string
-} 
+  projectType: (typeof projects)[number]
+  packageManager: (typeof packageManagers)[number]
+  name: string
+}
 
 export default function projectStarter(options: options) {
-    switch (options.projectType) {
-        case "react-ts-tw":
-            createReactProject({ name: options.name, packageManager: options.packageManager })
-            break;
-        case "next-ts":
-            console.log("next-ts", options.packageManager, options.name)
-            break;
-        case "wxt-react":
-            console.log("wxt-react", options.packageManager, options.name)
-            break;
-        case "wxt-svelte":
-            console.log("wxt-svelte", options.packageManager, options.name)
-            break;
-    }
+  switch (options.projectType) {
+    case "react-ts-tw":
+      createReactProject({ name: options.name, packageManager: options.packageManager })
+      break
+    case "next-ts":
+      console.log("next-ts", options.packageManager, options.name)
+      break
+    case "wxt-react":
+      console.log("wxt-react", options.packageManager, options.name)
+      break
+    case "wxt-svelte":
+      console.log("wxt-svelte", options.packageManager, options.name)
+      break
+  }
 }

7e244dfefdb85836457011a93d50ee1d91a5d5af
Burak Unver <burakhanunver@gmail.com>
2025-07-12 18:59:29 +0300

    move file, add approve builds support for pnpm

---
+++ src/starters/create-react.ts
@@ -0,0 +1,42 @@
+import { spawn } from "node:child_process"
+import { installDependencies } from "../helpers/install-deps"
+import { approveBuilds } from "../helpers/pnpm-approve"
+
+type projectOptions = {
+  name: string
+  packageManager: string
+}
+
+// create react-ts project
+export async function createReactProject(options: projectOptions) {
+  const { name, packageManager } = options
+  let command: string
+  switch (packageManager) {
+    case "pnpm":
+      command = `pnpm create vite ${name} --template react-swc-ts`
+      break
+    case "npm":
+      command = `npm create vite@latest ${name} -- --template react-swc-ts`
+      break
+    case "bun":
+      command = `bun create vite ${name} --template react-swc-ts`
+      break
+    default:
+      command = `pnpm create vite ${name} --template react-swc-ts`
+  }
+  const child = spawn(command, { shell: true })
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+  child.on("close", async code => {
+    if (code == 0) {
+      console.log(`Created React project at ${name}`)
+      // install dependencies
+      const dependencyInstallSuccess = await installDependencies(packageManager, name)
+      if (dependencyInstallSuccess && packageManager === "pnpm") {
+        approveBuilds()
+      }
+    } else {
+      console.error(`Failed to create React project at ${name}`)
+    }
+  })
+}

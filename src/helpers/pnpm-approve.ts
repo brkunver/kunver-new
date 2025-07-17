85dc5192074058d8e39c2a46aaebe8c29ab49e4b
Burak Unver <burakhanunver@gmail.com>
2025-07-17 17:01:32 +0300

    add project path

--- src/helpers/pnpm-approve.ts
+++ src/helpers/pnpm-approve.ts
@@ -1,18 +1,14 @@
 import { spawn } from "node:child_process"
 import ora from "ora"
 
-export async function approveBuilds() {
+export async function approveBuilds(projectName: string) {
   return new Promise(resolve => {
-    const child = spawn("pnpm approve-builds", { shell: true })
+    const command = "cd " + projectName + " && pnpm approve-builds"
+    const child = spawn(command, { shell: true, cwd: process.cwd() })
     const spinner = ora("Approving builds").start()
-    spinner.color = "blue"
+    
     child.stdout.pipe(process.stdout)
     child.stderr.pipe(process.stderr)
-    // wait for 500ms
-    setTimeout(() => {
-      child.stdin.write("a")
-      child.stdin.write("y")
-    }, 500)
     child.on("close", code => {
       if (code === 0) {
         spinner.succeed("Builds approved.")

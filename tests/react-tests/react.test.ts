299f8f1262b90bda4d878e1082dbb3c038375e3b
Burak Unver <burakhanunver@gmail.com>
2025-07-17 18:30:21 +0300

    add before and after

--- tests/react-tests/react.test.ts
+++ tests/react-tests/react.test.ts
@@ -1,16 +1,24 @@
 import { createTempDir, removeTempDir } from "../tools/tempdir"
 import { createReactProject } from "../../src/starters/create-react"
 import chalk from "chalk"
-import { expect, it } from "vitest"
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
 import fs from "node:fs/promises"
 import path from "node:path"
-
 
 const projectName = "testing-react"
 const packageManager = "pnpm"
 
-it("should create a react project", async () => {
-    const tempDir = await createTempDir()
+describe("create-react", () => {
+  let tempDir: string | undefined
+  beforeAll(async () => {
+    tempDir = await createTempDir()
+  })
+
+  afterAll(async () => {
+    await removeTempDir(tempDir)
+  })
+
+  it("should create a react project", async () => {
     if (!tempDir) throw new Error("Failed to create temp directory")
     console.log(chalk.green(`Temporary directory created at: ${tempDir}`))
 
@@ -18,11 +26,10 @@ it("should create a react project", async () => {
 
     const projectPath = path.join(tempDir, projectName)
     try {
-        await fs.access(projectPath)
-        expect(true)
+      await fs.access(projectPath)
+      expect(true)
     } catch (error) {
-        expect(false)
+      expect(false)
     }
-
-    await removeTempDir(tempDir)
-},30000)
+  }, 30000)
+})

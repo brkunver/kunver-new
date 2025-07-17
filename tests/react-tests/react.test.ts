7ccac1184f3d5b8113722acbb412b4887ed462ae
Burak Unver <burakhanunver@gmail.com>
2025-07-17 18:20:15 +0300

    add vitest

--- tests/react-tests/react.test.ts
+++ tests/react-tests/react.test.ts
@@ -1,20 +1,28 @@
 import { createTempDir, removeTempDir } from "../tools/tempdir"
 import { createReactProject } from "../../src/starters/create-react"
 import chalk from "chalk"
+import { expect, it } from "vitest"
 import fs from "node:fs/promises"
 import path from "node:path"
+
 
 const projectName = "testing-react"
 const packageManager = "pnpm"
 
-try {
+it("should create a react project", async () => {
     const tempDir = await createTempDir()
-    if (!tempDir) process.exit(1)
+    if (!tempDir) throw new Error("Failed to create temp directory")
     console.log(chalk.green(`Temporary directory created at: ${tempDir}`))
 
     await createReactProject({ name: projectName, packageManager })
 
+    const projectPath = path.join(tempDir, projectName)
+    try {
+        await fs.access(projectPath)
+        expect(true)
+    } catch (error) {
+        expect(false)
+    }
+
     await removeTempDir(tempDir)
-} catch (error) {
-    console.error(error)
-}
+},30000)

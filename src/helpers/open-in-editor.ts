af7fc816b654fc2761abac91a3cb2b6f3dd0851e
Burak Unver <burakhanunver@gmail.com>
2025-07-19 17:19:38 +0300

    change arg name

--- src/helpers/open-in-editor.ts
+++ src/helpers/open-in-editor.ts
@@ -3,10 +3,10 @@ import chalk from "chalk"
 
 type editorName = "code" | "windsurf" | "cursor"
 
-export async function openInEditor(name: string, cwd: string, editorName: editorName) {
-  console.log(chalk.blue(`Opening ${name} in ${editorName}`))
+export async function openInEditor(projectName: string, cwd: string, editorName: editorName) {
+  console.log(chalk.blue(`Opening ${projectName} in ${editorName}`))
   return new Promise(resolve => {
-    const child = spawn(`${editorName} ${name}`, { shell: true, cwd: cwd })
+    const child = spawn(`${editorName} ${projectName}`, { shell: true, cwd: cwd })
     child.on("close", () => resolve(true))
   })
 }

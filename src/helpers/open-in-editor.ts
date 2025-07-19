fceef7afe4e43fbafb9bf00d4434cb909655a082
Burak Unver <burakhanunver@gmail.com>
2025-07-19 17:16:47 +0300

    add chalk

--- src/helpers/open-in-editor.ts
+++ src/helpers/open-in-editor.ts
@@ -1,10 +1,10 @@
-import path from "node:path"
 import { spawn } from "node:child_process"
-// after everything, ask user to open the project in their editor
+import chalk from "chalk"
 
 type editorName = "code" | "windsurf" | "cursor"
 
 export async function openInEditor(name: string, cwd: string, editorName: editorName) {
+  console.log(chalk.blue(`Opening ${name} in ${editorName}`))
   return new Promise(resolve => {
     const child = spawn(`${editorName} ${name}`, { shell: true, cwd: cwd })
     child.on("close", () => resolve(true))

24a88be27251c63099f309d8cbc17519106cb603
Burak Unver <burakhanunver@gmail.com>
2025-07-20 17:07:21 +0300

    fix projectName

--- tests/react-tests/react.test.ts
+++ tests/react-tests/react.test.ts
@@ -26,7 +26,7 @@ async function readFileSafe(filePath: string): Promise<string> {
 
 // Test configuration
 describe.each(packageManagers)(`React project with %s`, async packageManager => {
-  const projectName = "testing-react-with " + packageManager
+  const projectName = "testing-react-with-" + packageManager
   let tempDir: string
   let projectPath: string
 

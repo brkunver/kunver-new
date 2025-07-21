8fd14af54dce22751416d084c3dc6d708be1a720
Burak Unver <burakhanunver@gmail.com>
2025-07-21 17:01:16 +0300

    add todo

--- src/starters/create-cpp.ts
+++ src/starters/create-cpp.ts
@@ -3,3 +3,6 @@ import { fileURLToPath } from "node:url"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+// TODO : copy cpp template folder
+// Create helper python script

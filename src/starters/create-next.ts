79f8af524904d579b4c5bae3c31dd1f3d234fc6c
Burak Unver <burakhanunver@gmail.com>
2025-07-21 16:34:31 +0300

    add main func

---
+++ src/starters/create-next.ts
@@ -0,0 +1,47 @@
+import path, { join } from "node:path"
+import { fileURLToPath } from "node:url"
+
+import { installDependencies } from "../helpers/install-deps"
+import { copyTemplateFolder } from "../helpers/copy-template"
+import { pnpmApproveBuilds, bunApproveBuilds } from "../helpers/approve"
+import { packageManagers } from "../project-starter"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+type projectOptions = {
+  name: string
+  packageManager: (typeof packageManagers)[number]
+  cwd?: string
+}
+
+export async function createNextProject(options: projectOptions) {
+  const { name, packageManager, cwd = process.cwd() } = options
+  // template path : dist/templates/next-prisma
+  const templatePath = join(__dirname, "templates", "next-prisma")
+  try {
+    // copy template folder
+    const isTemplateCopied = await copyTemplateFolder(templatePath, join(cwd, name))
+    if (!isTemplateCopied) {
+      throw new Error("Failed to copy template folder")
+    }
+
+    // Install dependencies
+    const isDepsInstalled = await installDependencies(packageManager, name, cwd)
+    if (!isDepsInstalled) {
+      throw new Error("Failed to install dependencies")
+    }
+
+    // Approve builds
+    if (packageManager === "pnpm") {
+      await pnpmApproveBuilds(name, cwd)
+    } else if (packageManager === "bun") {
+      await bunApproveBuilds(name, cwd)
+    }
+
+    return true
+  } catch (error) {
+    console.error("Error creating Next project:", error)
+    throw error
+  }
+}

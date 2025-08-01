35b11ab32e4800b94309d754b6fac3a0dab97d41
Burak Unver <burakhanunver@gmail.com>
2025-08-01 12:46:13 +0300

    remove banner

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,9 +5,6 @@ export default defineConfig({
   format: ["esm"],
   dts: false,
   clean: true,
-  banner: {
-    js: "#!/usr/bin/env node",
-  },
   splitting: false,
   minify: true,
   publicDir: "src/public",

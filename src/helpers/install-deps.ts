fc9210286d768699101b3ac061fd5d490b391e3c
Burak Unver <burakhanunver@gmail.com>
2025-08-25 18:12:35 +0300

    refactor : make execa write output

--- src/helpers/install-deps.ts
+++ src/helpers/install-deps.ts
@@ -1,18 +1,15 @@
 import { execa } from "execa"
 import chalk from "chalk"
-import ora from "ora"
 
 export async function installDependencies(packageManager: string, name: string, cwd: string): Promise<boolean> {
   let command: string = `cd ${name} && ${packageManager} install`
 
-  const spinner = ora("Installing dependencies with " + chalk.blue(packageManager)).start()
+  console.log("Installing dependencies with " + chalk.cyan(packageManager) + "\n")
 
   try {
-    await execa(command, { shell: true, cwd: cwd })
-    spinner.succeed("Installed dependencies for " + chalk.blue(name))
+    await execa(command, { shell: true, cwd: cwd, stdout: "inherit", stderr: "inherit" })
     return true
   } catch (error) {
-    spinner.fail("Failed to install dependencies for " + chalk.blue(name))
     return false
   }
 }

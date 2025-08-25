b48d4f1b043266dfa050a23029f52d2bd8f98bb4
Burak Unver <burakhanunver@gmail.com>
2025-08-25 16:35:15 +0300

    feat : export createTemplate helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,5 +3,6 @@ import { installDependencies } from "./install-deps"
 import { copyTemplateFolder } from "./copy-template"
 import { addManagerScript } from "./add-manager-script"
 import { openInEditor } from "./open-in-editor"
+import { createTemplateProject } from "./create-template"
 
-export { installDependencies, copyTemplateFolder, approveBuilds, addManagerScript, openInEditor }
+export { installDependencies, copyTemplateFolder, approveBuilds, addManagerScript, openInEditor, createTemplateProject }

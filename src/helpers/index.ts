7c6613e639cd702828eb607a29f3225b7666de79
Burak Unver <burakhanunver@gmail.com>
2025-08-25 15:22:55 +0300

    feat : export openInEditor helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,6 @@ import approveBuilds from "./approve"
 import { installDependencies } from "./install-deps"
 import { copyTemplateFolder } from "./copy-template"
 import { addManagerScript } from "./add-manager-script"
+import { openInEditor } from "./open-in-editor"
 
-export { installDependencies, copyTemplateFolder, approveBuilds, addManagerScript }
+export { installDependencies, copyTemplateFolder, approveBuilds, addManagerScript, openInEditor }

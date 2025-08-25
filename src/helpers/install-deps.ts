bb602af8868ab0c079542145a783358bb8b8c6a5
Burak Unver <burakhanunver@gmail.com>
2025-08-25 17:36:22 +0300

    refactor : simplify command

--- src/helpers/install-deps.ts
+++ src/helpers/install-deps.ts
@@ -3,21 +3,7 @@ import chalk from "chalk"
 import ora from "ora"
 
 export async function installDependencies(packageManager: string, name: string, cwd: string): Promise<boolean> {
-  let command: string
-
-  switch (packageManager) {
-    case "pnpm":
-      command = `cd ${name} && pnpm install`
-      break
-    case "npm":
-      command = `cd ${name} && npm install`
-      break
-    case "bun":
-      command = `cd ${name} && bun install`
-      break
-    default:
-      command = `cd ${name} && pnpm install`
-  }
+  let command: string = `cd ${name} && ${packageManager} install`
 
   const spinner = ora("Installing dependencies with " + chalk.blue(packageManager)).start()
 

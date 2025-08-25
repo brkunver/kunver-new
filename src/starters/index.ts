4932d279225c0de65a6dd2849014be4b6d47bad6
Burak Unver <burakhanunver@gmail.com>
2025-08-25 15:23:01 +0300

    Init

---
+++ src/starters/index.ts
@@ -0,0 +1,13 @@
+import { createElectronSvelteProject } from "./create-electron-svelte"
+import { createNextProject } from "./create-next"
+import { createPythonNotebookProject } from "./create-python-notebook"
+import { createReactProject } from "./create-react"
+import { createWxtProject } from "./create-wxt"
+
+export {
+  createElectronSvelteProject,
+  createNextProject,
+  createPythonNotebookProject,
+  createReactProject,
+  createWxtProject,
+}

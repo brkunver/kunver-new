e3f309ac002413aef6df2746d7a7b03b32f33924
Burak Unver <burakhanunver@gmail.com>
2025-08-25 15:02:02 +0300

    init

---
+++ src/helpers/index.ts
@@ -0,0 +1,6 @@
+import approveBuilds from "./approve"
+import { installDependencies } from "./install-deps"
+import { copyTemplateFolder } from "./copy-template"
+import { addManagerScript } from "./add-manager-script"
+
+export { installDependencies, copyTemplateFolder, approveBuilds, addManagerScript }

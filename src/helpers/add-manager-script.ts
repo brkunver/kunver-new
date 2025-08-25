f5ba9cb23c15c78d027034c8046d9e03addcdfd0
Burak Unver <burakhanunver@gmail.com>
2025-08-25 15:01:36 +0300

    make async

--- src/helpers/add-manager-script.ts
+++ src/helpers/add-manager-script.ts
@@ -3,11 +3,12 @@ import fs from "node:fs"
 
 export async function addManagerScript(packageManager: string, name: string, cwd: string) {
   const packageJsonPath = join(cwd, name, "package.json")
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"))
+  const packageJsonFile = await fs.promises.readFile(packageJsonPath, "utf-8")
+  const packageJson = JSON.parse(packageJsonFile)
   if (packageManager === "bun") {
     packageJson.scripts.manager = `bun manager.cjs ${packageManager}`
   } else {
     packageJson.scripts.manager = `node manager.cjs ${packageManager}`
   }
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+  await fs.promises.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2))
 }

b5e42abd4be1ad7369f89e0031962ca25fb0c6a7
Burak Unver <burakhanunver@gmail.com>
2025-08-25 17:31:07 +0300

    refactor : remove unused helpers

--- src/starters/index.ts
+++ src/starters/index.ts
@@ -1,13 +1,4 @@
-import { createElectronSvelteProject } from "./create-electron-svelte"
-import { createNextProject } from "./create-next"
 import { createPythonNotebookProject } from "./create-python-notebook"
-import { createReactProject } from "./create-react"
 import { createWxtProject } from "./create-wxt"
 
-export {
-  createElectronSvelteProject,
-  createNextProject,
-  createPythonNotebookProject,
-  createReactProject,
-  createWxtProject,
-}
+export { createPythonNotebookProject, createWxtProject }
